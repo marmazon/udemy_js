ec367947de9c64fd76c8f493d8dc0956ed61138d
Yevhen <marmazone@gmail.com>
2020-04-01 19:08:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+
+// console.log(7/0);
+// let persone = {
+//     name: "Yevhen",
+//     age:39,
+//     isMarried: true
+// };
+// console.log(persone);
+// console.log(persone.name);
+// console.log("Is married? " + persone.isMarried);
+
+// let array = ['apple', 'huyakle', 'cucumber'];
+// console.log(array[0]);
+
+// // alert("Hello!");
+// let answer = confirm("Leave this page?");
+// // let answer = prompt("How are you?","Not bad");
+// console.log(typeof(false));
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+// console.log(++incr);
+// console.log(--decr);
+// console.log("2" === 2);
+
+let isChecked = false,
+    isClose = true;
+
+console.log(!isChecked && isClose);
