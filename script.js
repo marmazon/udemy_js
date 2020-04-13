c383e1f8df2a482a04bd620c13c62fe4ce6b9467
Yevhen <marmazone@gmail.com>
2020-04-13 15:24:21 +0300

    Three types of cycles

--- script.js
+++ script.js
@@ -12,13 +12,54 @@ let appData = {
     savings: false
 };
 
-console.log(money);
-console.log(time);
 
-let requiredCostName= prompt("Enter a required cost item this month"),
-    requiredCostValue= prompt("How much will it cost?") ;
+// for(let i=1; i<=2; i++){
+    // let requiredCostName= prompt("Enter a required cost item this month"),
+    //     requiredCostValue= +prompt("How much will it cost?") ;
 
-appData.expenses[requiredCostName] = requiredCostValue;
-console.log(appData.expenses[requiredCostName]);
+    // if(typeof(requiredCostName) != 'string' || requiredCostName.length > 50 || typeof(requiredCostValue) != 'number' 
+    // || requiredCostValue == '' || requiredCostName == ''){
+    //     alert('Error! More then 50 symbols or empty'); 
+    //     i--;
+    // } else {
+    //     appData.expenses[requiredCostName] = requiredCostValue;
+    // }
+    // i++;
+
+    
+// }
+
+// let i=1; 
+// while( i <= 2 ){
+//     let requiredCostName= prompt("Enter a required cost item this month"),
+//         requiredCostValue= +prompt("How much will it cost?") ;
+
+//     if(typeof(requiredCostName) != 'string' || requiredCostName.length > 50 || typeof(requiredCostValue) != 'number' 
+// || requiredCostValue == '' || requiredCostName == ''){
+//         alert('Error! More then 50 symbols or empty'); 
+//         i--;
+//     } else {
+//         appData.expenses[requiredCostName] = requiredCostValue;
+//     }
+//     i++;
+// }
+
+
+let i=1; 
+do{
+    let requiredCostName= prompt("Enter a required cost item this month"),
+        requiredCostValue= +prompt("How much will it cost?") ;
+
+    if(typeof(requiredCostName) != 'string' || requiredCostName.length > 50 || typeof(requiredCostValue) != 'number' ||
+     requiredCostValue == '' || requiredCostName == ''){
+        alert('Error! More then 50 symbols or empty'); 
+        i--;
+    } else {
+        appData.expenses[requiredCostName] = requiredCostValue;
+    }
+    i++;
+}
+while( i <= 2 );
+
 
 alert("Daily budget: "+ appData.budget/30);
