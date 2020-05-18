33ebfc1202abfad2632557db3d8715001dc226de
Yevhen <marmazone@gmail.com>
2020-05-18 18:48:41 +0300

    Urok2_new Cycles and condition

--- 1urok_new/script.js
+++ 1urok_new/script.js
@@ -1,20 +1,33 @@
 'use strict';
 
-let numberOfFilms;
+const numberOfFilms = +prompt("Скололько фильмов вы уже просмотрели?", "");
 
-numberOfFilms = prompt("Скололько фильмов вы уже просмотрели?");
-
-let personalMovieBD =
-{
+const personalMovieBD = {
     count : numberOfFilms,
     movies : {},
     actors : {},
-    genres: {},
+    genres: [],
     privat: false
 };
 
 for (let i = 1; i<=2; i++ ){
-    const movieName = prompt("Один из последних просмотренных фильмов?");
-    const movieRatingByUser = prompt("На сколько оцените его?");
+    const movieName = prompt("Один из последних просмотренных фильмов?", "");
+    if (movieName === null || movieName == "" || movieName.length>50 ){
+        i--;
+        continue;
+    }
+
+    const movieRatingByUser = prompt("На сколько оцените его?", "");
+    if (movieRatingByUser === null || movieRatingByUser == "" || movieRatingByUser.length>50 ){
+        i--;
+        continue;
+    }
     personalMovieBD.movies[movieName] = movieRatingByUser;
+}
+
+if(personalMovieBD.count<10){
+    console.log("Просмотрено мало фильмов.");
+}else if(personalMovieBD.count>=10 && personalMovieBD.count<=30){
+    console.log("Вы класный зритель!");
+} else {console.log("Вы киноман!");
 }
