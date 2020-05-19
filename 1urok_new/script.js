41d1caaa0c2c9bfd43447863d2a009a67d593a8b
Yevhen <marmazone@gmail.com>
2020-05-19 19:41:05 +0300

    Urok3_new functions

--- 1urok_new/script.js
+++ 1urok_new/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const numberOfFilms = +prompt("Скололько фильмов вы уже просмотрели?", "");
+let numberOfFilms;
+
+start();
 
 const personalMovieBD = {
     count : numberOfFilms,
@@ -10,7 +12,22 @@ const personalMovieBD = {
     privat: false
 };
 
-for (let i = 1; i<=2; i++ ){
+
+rememberMyFilms();
+detectPersonalLevel();
+showMyDB();
+
+
+
+function start() {
+   numberOfFilms = prompt("Скололько фильмов вы уже просмотрели?", "");
+   while (numberOfFilms=='' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = prompt("Скололько фильмов вы уже просмотрели?", "");
+   }
+}
+
+function rememberMyFilms() {
+    for (let i = 1; i<=2; i++ ){
     const movieName = prompt("Один из последних просмотренных фильмов?", "");
     if (movieName === null || movieName == "" || movieName.length>50 ){
         i--;
@@ -24,10 +41,28 @@ for (let i = 1; i<=2; i++ ){
     }
     personalMovieBD.movies[movieName] = movieRatingByUser;
 }
+}
 
-if(personalMovieBD.count<10){
-    console.log("Просмотрено мало фильмов.");
-}else if(personalMovieBD.count>=10 && personalMovieBD.count<=30){
-    console.log("Вы класный зритель!");
-} else {console.log("Вы киноман!");
+function detectPersonalLevel(){
+    if(personalMovieBD.count>=0 && personalMovieBD.count<10){
+        console.log("Просмотрено мало фильмов.");
+    }else if(personalMovieBD.count>=10 && personalMovieBD.count<=30){
+        console.log("Вы классический зритель!");
+    } else if(personalMovieBD.count>30){
+        console.log("Вы киноман!");}
+    else{
+        console.log("Ошибка");
+    }
+}
+
+function showMyDB() {
+    if(!personalMovieBD.privat){
+        console.log(personalMovieBD);
+    }
+}
+writeYourGenres();
+function writeYourGenres() {
+    for (let i=0; i<=2; i++){
+        personalMovieBD.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`); 
+    }
 }
